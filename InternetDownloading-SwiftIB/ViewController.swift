c21b9e6965a1993b41c94dc80a5d265da3420764
Alistair Cooper <alistair@alistaircooper.com>
2016-05-24 00:34:59 -0700

    added spinner indicators and NSURL Session code

--- InternetDownloading-SwiftIB/ViewController.swift
+++ InternetDownloading-SwiftIB/ViewController.swift
@@ -8,12 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var activityIndicator1: UIActivityIndicatorView!
+    
+    var downloadTask: NSURLSessionDownloadTask!
+    var backgroundSession: NSURLSession!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        activityIndicator1.startAnimating()
+        
+        // start network pinwheel
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        let url = NSURL(string: "https://s3-us-west-2.amazonaws.com/uclaiosclass/acPiano.jpg")!
+        let session = NSURLSession.sharedSession()  // NOTE sharedSession is for basic requests
+        
+        let downloadTask = session.dataTaskWithURL(url) { data, response, error in
+            if data == nil {
+                print("\(error)")
+                
+            } else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.imageView1.image = UIImage(data: data!)
+                    self.imageView1.contentMode = UIViewContentMode.ScaleAspectFit
+                    
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                    
+                    self.activityIndicator1.stopAnimating()
+                }
+                
+                
+            }
+            
+        }
+        
+        downloadTask.resume()
+
+        
+
+        
+    
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
