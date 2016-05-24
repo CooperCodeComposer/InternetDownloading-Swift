7a8eb7da7f6d5bf18548fc2951be58336a66e3e6
Alistair Cooper <alistair@alistaircooper.com>
2016-05-23 19:22:54 -0700

    Initial Commit

---
+++ InternetDownloading-SwiftIB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InternetDownloading-SwiftIB
+//
+//  Created by Alistair Cooper on 5/23/16.
+//  Copyright © 2016 Alistair Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
