135999415544f5ee0c020a95c7150047234fc1cf
Alistair Cooper <cooper@swiftcodecomposer.com>
2016-09-11 19:57:30 -0700

    initial commit

--- InternetDownloading-SwiftIB/ViewController.swift
+++ InternetDownloading-SwiftIB/ViewController.swift
@@ -65,13 +65,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         downloadTask.resume()
         
     }
-    
 
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
